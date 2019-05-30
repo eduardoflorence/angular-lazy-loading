d2bff08076fa6ecc2c4bafd9bfd2c22c67488bf1
Eduardo Florence <eduardo@geminisistemas.com>
2019-05-30 13:55:22 -0300

    Uso de Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ClientesComponent } from './clientes/clientes.component';
-import { FornecedoresComponent } from './fornecedores/fornecedores.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'clientes', component: ClientesComponent },
-  { path: 'fornecedores', component: FornecedoresComponent },
+  { path: 'clientes', loadChildren: './clientes/clientes.module#ClientesModule' },
+  { path: 'fornecedores', loadChildren: './fornecedores/fornecedores.module#FornecedoresModule' }
 ];
 
 @NgModule({
--- src/app/clientes/clientes-routing.module.ts
+++ src/app/clientes/clientes-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientesComponent } from './clientes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ClientesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
