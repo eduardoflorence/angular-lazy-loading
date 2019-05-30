7e69182a02b562f4360eaffd09f44d404440960d
Eduardo Florence <eduardo@geminisistemas.com>
2019-05-30 14:57:45 -0300

    Estratégia de pré-carregamento com delay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,11 @@ import { CustomPreloaderService } from './custom-preloader.service';
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'clientes', loadChildren: './clientes/clientes.module#ClientesModule', data: {preload: true} },
+  {
+    path: 'clientes',
+    loadChildren: './clientes/clientes.module#ClientesModule',
+    data: {preload: true, delay: 500}
+  },
   { path: 'fornecedores', loadChildren: './fornecedores/fornecedores.module#FornecedoresModule' }
 ];
 
--- src/app/custom-preloader.service.ts
+++ src/app/custom-preloader.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { PreloadingStrategy, Route } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import { Observable, of, timer } from 'rxjs';
+import { flatMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,15 @@ import { Observable, of } from 'rxjs';
 export class CustomPreloaderService implements PreloadingStrategy {
 
   preload(route: Route, load: () => Observable<any>): Observable<any> {
-    return route.data && route.data.preload ? load() : of(null);
+    // return route.data && route.data.preload ? load() : of(null);
+
+    const loadRoute = (delay: number) => delay
+        ? timer(delay).pipe(flatMap(() => load()))
+        : load();
+
+    return route.data && route.data.preload
+        ? loadRoute(route.data.delay)
+        : of(null);
   }
 
 }
